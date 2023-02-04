636190a4600edbd08db81fab9c816cac13176b0d
Tejas Udupa <tejasudupa1285@gmail.com>
2023-02-04 10:15:32 +0530

    ws_client: don't include packet.hpp instead include json-schema.hpp
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- compute_unit/socket/ws_client.hpp
+++ compute_unit/socket/ws_client.hpp
@@ -1,7 +1,9 @@
 #ifndef WS_CLIENT_H
 #define WS_CLIENT_H
 #include<string>
-#include "packet.hpp"
+#include "../lib/nlohmann/json-schema.hpp"
+
+using json = nlohmann::json;
 
 extern json ws_client_launch(struct socket*, json);
 
