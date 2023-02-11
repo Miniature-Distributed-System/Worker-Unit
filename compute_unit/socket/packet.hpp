d4c8b750d91dd26adfab072e46ae193bf5668e65
Tejas Udupa <tejasudupa1285@gmail.com>
2023-02-11 19:35:49 +0530

    packet.hpp: refactor client status code
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- compute_unit/socket/packet.hpp
+++ compute_unit/socket/packet.hpp
@@ -4,12 +4,16 @@
 #include "json.hpp"
 using json = nlohmann::json;
 
-#define P_RESET         1 << 0 
-#define P_HANDSHAKE     1 << 1 //Inital handshake
-#define P_RSEND_DATA    1 << 2 //Resend data due to failure
-#define P_RECV_DATA     1 << 3 //Data received correctly
-#define P_DATSENT       1 << 4 //Data sent to server
-#define P_SEIZE         1 << 5 //Don't send any more packets CU at limit
+enum compute_packet_status {
+    P_RESET     = 1 << 0, //Empty packet essentially
+    P_HANDSHAKE = 1 << 1, //Handshake snd,ack packet
+    P_DATA_ACK  = 1 << 2, //Table received ack
+    P_INTR_RES  = 1 << 3, //Intermediate result sent
+    P_FINAL_RES = 1 << 4, //Final result inside packet
+    P_ERR       = 1 << 5, //Packet error
+    P_QSEND     = 1 << 6, //Client is in quick send mode
+    P_SEIZE     = 1 << 7, //Client has seized receiving packets
+};
 
 enum packet_code {
     NO_ERR = 0,
