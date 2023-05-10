883cfe5a22020fb3cba66d527277d9d5443f1cd5
Tejas Udupa <tejasudupa1285@gmail.com>
2023-05-10 10:35:34 +0530

    Add Statistics Engine for collecting worker data
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

---
+++ services/stats_engine.cpp
@@ -0,0 +1,109 @@
+#include "stats_engine.hpp"
+#include "../include/debug_rp.hpp"
+
+StatisticsEngine::StatisticsEngine(int threadCount, int statisticsArraySize) : threadCount(threadCount),
+    statisticsArraySize(statisticsArraySize)
+{
+    sem_init(&calcAvgLock, 0, 1);
+    sem_init(&timerAfterLock, 0, 1);
+    sem_init(&timerBeforeLock, 0, 1);
+    sem_init(&taskStatsUpdateLock, 0 ,1);
+    for(int i = 0; i <= NR_TASK_PRIORITY; i++)
+        taskCountVector.push_back(0);
+    averageQueueTime = 0;
+    DEBUG_MSG(__func__, "inited stats engine");
+}
+
+/* calculateAvgTimerVector(): Takes the whole of queueTimeVector and averages all its values and updates 
+ * averageQueueTime member variable.
+ * Desc: This method gives us the average of the all times recorded for the updateTimerBefore() and updateTimerAfter()
+ * This gives us an idea of how much the worker is capable of taking in tasks. It tells us if worker is getting
+ * overwhelemed with tasks and the server needs to hold off tasks for a while for worker to finish the pending tasks.
+*/
+void StatisticsEngine::calculateAvgTimerVector()
+{
+    int totalSize = queueTimeVector.size();
+    double timerAggrigate = 0;
+    double finalAverage;
+
+    if(queueTimeVector.size() == 0)
+        return;
+
+    sem_wait(&calcAvgLock);
+    for(auto i = queueTimeVector.begin(); i != queueTimeVector.end(); i++){
+        timerAggrigate += *i;
+    }
+    finalAverage = timerAggrigate / totalSize;
+    queueTimeVector.clear();
+    queueTimeVector.push_back(finalAverage);
+    averageQueueTime = finalAverage;
+    sem_post(&calcAvgLock);
+}
+
+/* updateTimerBefore(): Updates the before timer when task is pushed into the thread pool
+*/
+void StatisticsEngine::updateTimerBefore()
+{
+    sem_wait(&timerBeforeLock);
+    timeBefore.push_back(std::chrono::high_resolution_clock::now());
+    sem_post(&timerBeforeLock);
+}
+
+/* updateTimerAfter(): Updates the after timer when task is popped from thread poo and calculates the diffrence of 
+*  after and before timer
+*  Desc: The values from above method are used and this method calculates the time taken by the thread between pushing
+*  new element in and popping another element. It essentaally gives stats of frequently a item gets in/out longer means
+*  the worker unit has more task or is struggling with tasks due to thread limits or processor performance.
+*/
+void StatisticsEngine::updateTimerAfter()
+{
+    sem_wait(&timerAfterLock);
+    timeAfter.push_back(std::chrono::high_resolution_clock::now());
+    for(auto i = timeAfter.begin(), j = timeBefore.begin(); i != timeAfter.end(); i++, j++){
+        // Caculate the average and go forward
+        if(queueTimeVector.size() >= statisticsArraySize)
+            calculateAvgTimerVector();
+        queueTimeVector.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(*i - *j).count());
+    }
+    sem_post(&timerAfterLock);
+}
+
+/* updateThreadStats(): Increment the counter for the repective task type
+ * Desc: This method updates the counter for each task type which is divided based on priority. It counts how many 
+ * tasks of a certain priority exists in the task pool currently.
+*/
+void StatisticsEngine::updateThreadStats(TaskPriority taskPriority, TaskStatus taskStatus)
+{
+    sem_wait(&taskStatsUpdateLock);
+    if(taskStatus == TASK_IN){
+         taskCountVector[taskPriority]++;
+    } else {
+        if(taskCountVector[taskPriority])
+            taskCountVector[taskPriority]--;
+    }
+    sem_post(&taskStatsUpdateLock);
+}
+
+/* toJson(); Converts the above values recorded to json to be exported to server
+*/
+json StatisticsEngine::toJson()
+{
+    json subPacket;
+    std::string thread;
+    
+    // Get current task queue map
+    subPacket["stats"];
+    thread = std::to_string(taskCountVector[0]);
+    for(int i = 1;i < taskCountVector.size(); i++)
+        thread += "," + std::to_string(taskCountVector[i]);
+    subPacket["stats"]["taskQueue"] = thread;
+
+    // Get the average queue milliseconds
+    if(!averageQueueTime)
+        calculateAvgTimerVector();
+    subPacket["stats"]["avgQueueTime"] = averageQueueTime;
+
+    subPacket["stats"]["totalAvailableThreads"] = threadCount;
+
+    return subPacket["stats"];
+}
---
+++ services/stats_engine.hpp
@@ -0,0 +1,37 @@
+#ifndef STATS_H
+#define STATS_H
+#include <vector>
+#include <semaphore.h>
+#include <chrono>
+#include "../include/task.hpp"
+#include "../include/flag.h"
+#include "../lib/nlohmann/json-schema.hpp"
+using json = nlohmann::json;
+
+enum TaskStatus {
+    TASK_IN,
+    TASK_OUT
+};
+
+class StatisticsEngine {
+        sem_t taskStatsUpdateLock, timerBeforeLock, timerAfterLock, calcAvgLock;
+        int statisticsArraySize;
+        int threadCount;
+        double averageQueueTime;    // Holds the average value of time vector
+        std::vector<int> taskCountVector;   // Gives the current task map in thread queue
+        std::vector<int> queueTimeVector;   // Stores average times of thread tasks 
+        std::vector<std::chrono::high_resolution_clock::time_point> timeBefore;
+        std::vector<std::chrono::high_resolution_clock::time_point> timeAfter;
+    public:
+        StatisticsEngine(){};
+        StatisticsEngine(int threadCount, int statisticsArraySize);
+        void updateTimerBefore();
+        void updateTimerAfter();
+        void calculateAvgTimerVector();
+        void updateThreadStats(TaskPriority taskPriority, TaskStatus taskStatus);
+        json toJson();
+};
+
+extern StatisticsEngine statsEngine;
+
+#endif
