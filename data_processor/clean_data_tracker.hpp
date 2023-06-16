bb17ca84d11dde3bd300dff0729d76380534563e
Tejas Udupa <tejasudupa1285@gmail.com>
2023-06-16 11:19:26 +0530

    data_processor: Expose `update_clean_stage()` to data cleaner
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- data_processor/clean_data_tracker.hpp
+++ data_processor/clean_data_tracker.hpp
@@ -5,6 +5,7 @@
 #include "../include/process.hpp"
 
 void schedule_clean_phase(std::string tableName);
+int update_clean_stages(TableData *tableData);
 extern std::map<TableData *, int> cleanStageMap;
 
 #endif
