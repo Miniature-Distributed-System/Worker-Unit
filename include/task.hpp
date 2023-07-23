0cf6e04f47ef8cddc1c1a7dcd8480f49c6917e71
Tejas Udupa <tejasudupa1285@gmail.com>
2023-07-23 11:18:18 +0530

    task: Fix `JobStatus` duplicate flags
    Signed-off-by: Tejas Udupa <tejasudupa1285@gmail.com>

--- include/task.hpp
+++ include/task.hpp
@@ -3,13 +3,12 @@
 
 /// This type is used for setting current job state in the pipeline
 enum JobStatus {
-    JOB_DONE        = 1 << 0,
-    JOB_RUNNING     = 1 << 1,
-    JOB_PENDING     = 1 << 2,
-    JOB_WAITING     = 1 << 3,
-    JOB_FAILED      = 1 << 4,
-    JOB_FINALIZED   = 1 << 5,
-    JOB_DONE        = 1 << 6
+    JOB_RUNNING     = 1 << 0,
+    JOB_PENDING     = 1 << 1,
+    JOB_WAITING     = 1 << 2,
+    JOB_FAILED      = 1 << 3,
+    JOB_FINALIZED   = 1 << 4,
+    JOB_DONE        = 1 << 5
 };
 
 enum TaskPriority {
